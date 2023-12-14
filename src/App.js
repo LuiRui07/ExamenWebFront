29deeac25b674923a2bef6f46deb113a01ae6851
Luis Ruiz <100932068+LuiRui07@users.noreply.github.com>
2023-12-14 13:07:34 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+
 
 function App() {
+  const [usuarios, setUsuarios] = useState([]);
+
+  useEffect(() => {
+    const obtenerUsuarios = async () => {
+      try {
+        const response = await fetch('http://localhost:5001/api');
+        if (response.ok) {
+          const data = await response.json();
+          setUsuarios(data);
+        } else {
+          console.error('Error al obtener Usuarios:', response.statusText);
+        }
+      } catch (error) {
+        console.error('Error al obtener Usuarios:', error);
+      }
+    };
+
+    obtenerUsuarios();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
           Hermano aqui estamos
         </p>
+        <ul>
+          {usuarios.map(producto => (
+            <li key={producto.id}>{producto.nombre}</li>
+          ))}
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -15,6 +43,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          
         </a>
       </header>
     </div>
